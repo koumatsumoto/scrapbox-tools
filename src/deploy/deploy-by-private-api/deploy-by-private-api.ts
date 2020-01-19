165526b3ce27bb9c8d572e8cf4893c1975c39786
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-01-19 23:22:41 +0900

    fix(deploy): correct settings page url and browser closing (#75)

--- src/deploy/deploy-by-private-api/deploy-by-private-api.ts
+++ src/deploy/deploy-by-private-api/deploy-by-private-api.ts
@@ -6,9 +6,8 @@ import { updateScriptText } from './internal/update-script-text';
 
 const applicationBootstrapWaitTime = 1000 * 8;
 
-export const deployByPrivateApi = async (param: { url: string; codeName: string; text: string }) => {
-  const browser = await puppeteer.launch({ headless: !config.local });
-  const context = await getFullPermissionBrowserContext(browser, config.origin);
+export const deployByPrivateApi = async (param: { browser: puppeteer.Browser; url: string; codeName: string; text: string }) => {
+  const context = await getFullPermissionBrowserContext(param.browser, config.origin);
   const page = await getConfiguredPage(context);
 
   // wait for react bootstrapped
@@ -19,7 +18,4 @@ export const deployByPrivateApi = async (param: { url: string; codeName: string;
   await loadBrowserJavaScript(page);
   // update script text
   await updateScriptText(page, param.codeName, param.text);
-
-  await page.close();
-  await browser.close();
 };
--- src/deploy/main.ts
+++ src/deploy/main.ts
@@ -1,6 +1,7 @@
 // load .env file for development in local
 require('dotenv').config();
 
+import * as puppeteer from 'puppeteer';
 import { config } from './config';
 import { deployByPrivateApi } from './deploy-by-private-api/deploy-by-private-api';
 import { loadUserCSS, loadUserScript } from './file-loaders';
@@ -8,12 +9,15 @@ import { loadUserCSS, loadUserScript } from './file-loaders';
 (async () => {
   const userPageText = await loadUserScript();
   const settingsPageText = await loadUserCSS();
+  const browser = await puppeteer.launch({ headless: !config.local });
 
   // deploy user script and user css
   await Promise.all([
-    deployByPrivateApi({ url: config.userPageUrl, codeName: 'script.js', text: userPageText }),
-    deployByPrivateApi({ url: config.userPageUrl, codeName: 'style.css', text: settingsPageText }),
+    deployByPrivateApi({ browser, url: config.userPageUrl, codeName: 'script.js', text: userPageText }),
+    deployByPrivateApi({ browser, url: config.settingsPageUrl, codeName: 'style.css', text: settingsPageText }),
   ]);
+
+  await browser.close();
 })()
   .then(() => {
     console.log('deploy completed');
