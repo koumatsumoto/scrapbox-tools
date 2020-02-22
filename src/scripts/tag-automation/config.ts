fa22a0fb37d7053b67b0368d17bf95059ebf5e58
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-02-22 15:41:57 +0900

    feat(tag-automation): arrange tag options (#128)

--- src/scripts/tag-automation/config.ts
+++ src/scripts/tag-automation/config.ts
@@ -3,7 +3,7 @@ export type TagOption = {
 }[];
 
 export const tagOptions: TagOption[] = [
-  [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'S' }, { value: 'T' }],
-  [{ value: '想起' }, { value: '体験' }, { value: '学習' }, { value: '作業' }],
-  [{ value: '読書' }, { value: '視聴' }, { value: '聴楽' }, { value: '閲覧' }],
+  [{ value: 'S' }, { value: 'T' }, { value: 'A' }, { value: 'B' }, { value: 'C' }],
+  [{ value: '読書' }, { value: '聴楽' }, { value: '視聴' }, { value: '閲覧' }, { value: '体験' }],
+  [{ value: '学習' }, { value: '想起' }],
 ];
