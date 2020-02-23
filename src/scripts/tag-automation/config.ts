61cb41d43d1895ff68fdf17f14321f10626510fb
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-02-23 17:39:40 +0900

    feat(scripts/tag-automation): allow edit tags manually (#131)

--- src/scripts/tag-automation/config.ts
+++ src/scripts/tag-automation/config.ts
@@ -4,6 +4,6 @@ export type TagOption = {
 
 export const tagOptions: TagOption[] = [
   [{ value: 'S' }, { value: 'T' }, { value: 'A' }, { value: 'B' }, { value: 'C' }],
-  [{ value: '読書' }, { value: '聴楽' }, { value: '視聴' }, { value: '閲覧' }, { value: '体験' }],
+  [{ value: '読書' }, { value: '聴楽' }, { value: '視聴' }, { value: '閲覧' }],
   [{ value: '学習' }, { value: '想起' }],
 ];
--- src/scripts/tag-automation/my-tag-form-dialog/my-tag-form-dialog.component.ts
+++ src/scripts/tag-automation/my-tag-form-dialog/my-tag-form-dialog.component.ts
@@ -1,17 +1,36 @@
 import { removeElement } from '../../../libs/common/dom';
 import { TagOption } from '../config';
-
 const html = require('./my-tag-form-dialog.component.html');
+import { addWord, removeWord, splitWords } from './textarea-operation';
 
 type CustomDialogResult<T> = { ok: false } | { ok: true; data: T };
+
+const makeCheckboxesHTML = (tagOptions: TagOption[]) => {
+  let labelId = 0;
+
+  return tagOptions
+    .map((group) => {
+      const inputs = group
+        .map((obj) => {
+          const id = `tag-${labelId++}`;
+          const value = obj.value;
+
+          return `<input id="${id}" type="checkbox" name="tags" value="${value}"><label for="${id}">${value}</label>`;
+        })
+        .join('');
+
+      return `<div class="tag-group">${inputs}</div>`;
+    })
+    .join('');
+};
 
 export class MyTagFormDialog extends HTMLElement {
   static readonly elementName = 'my-tag-form-dialog';
   private dialogElement: HTMLDialogElement;
   private checkboxesContainerElement: HTMLDivElement;
-  private formElement: HTMLFormElement;
-  // for checkbox id
-  private labelId = 0;
+  private inputTextElement: HTMLInputElement;
+  // this component can emit result only once.
+  // after emission, component will be removed from <body> and destroyed.
   private dialogCloseResult: Promise<CustomDialogResult<string[]>>;
 
   constructor(tagOptions: TagOption[]) {
@@ -19,19 +38,30 @@ export class MyTagFormDialog extends HTMLElement {
 
     this.innerHTML = `${html}`;
     this.dialogElement = this.querySelector<HTMLDialogElement>('dialog')!;
-    this.formElement = this.querySelector<HTMLFormElement>('form')!;
+    this.inputTextElement = this.querySelector<HTMLInputElement>('input[type=text]')!;
+
     // construct DOM of checkboxes
-    this.checkboxesContainerElement = this.querySelector<HTMLDivElement>('div.checkboxes')!;
-    this.checkboxesContainerElement.innerHTML = this.makeCheckboxesHTML(tagOptions);
+    this.checkboxesContainerElement = this.querySelector<HTMLDivElement>('#checkboxContainer')!;
+    this.checkboxesContainerElement.innerHTML = makeCheckboxesHTML(tagOptions);
+    for (const e of this.querySelectorAll<HTMLInputElement>('input[type=checkbox]')) {
+      e.addEventListener('change', () => {
+        if (e.checked) {
+          addWord(e.value, this.inputTextElement);
+          this.inputTextElement.value = this.inputTextElement.value + ' ';
+        } else {
+          removeWord(e.value, this.inputTextElement);
+        }
+      });
+    }
+
     // button handling
     const cancelButton = this.querySelector<HTMLButtonElement>('button[value=cancel]')!;
     const submitButton = this.querySelector<HTMLButtonElement>('button[value=default]')!;
-    this.dialogCloseResult = new Promise<CustomDialogResult<string[]>>((resolve, reject) => {
+    this.dialogCloseResult = new Promise<CustomDialogResult<string[]>>((resolve) => {
       submitButton.addEventListener(
         'click',
         () => {
-          const values = this.retrieveFormValues();
-          resolve({ ok: true, data: values });
+          resolve({ ok: true, data: splitWords(this.inputTextElement.value) });
           this.dialogElement.close();
           removeElement(this);
         },
@@ -53,29 +83,5 @@ export class MyTagFormDialog extends HTMLElement {
     this.dialogElement.showModal();
 
     return this.dialogCloseResult;
-  }
-
-  private makeCheckboxesHTML(tagOptions: TagOption[]) {
-    return tagOptions
-      .map((group) => {
-        const inputs = group
-          .map((obj) => {
-            const id = `usd-${this.labelId++}`;
-            const value = obj.value;
-
-            return `<input id="${id}" type="checkbox" name="tags" value="${value}"><label for="${id}">${value}</label>`;
-          })
-          .join('');
-
-        return `<div class="tag-group">${inputs}</div>`;
-      })
-      .join('');
-  }
-
-  private retrieveFormValues() {
-    const formData = new FormData(this.formElement);
-    const checked = formData.getAll('tags') as string[];
-
-    return checked.filter((str) => 0 < str.length);
   }
 }
