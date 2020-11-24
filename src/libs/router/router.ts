b393bdb372d8b7979265b2e8ed446461d5b20f3f
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-11-24 16:43:17 +0900

    update

--- src/libs/router/router.ts
+++ src/libs/router/router.ts
@@ -1,11 +1,13 @@
+import { Observable } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
 import { patchWindowHistoryApiAndGetEventStream } from './internal/patch-window-history-api-and-get-event-stream';
 import { isSameUrl } from './internal/predicate';
+import { RouterEvent } from './types';
 
 export class Router {
   private readonly stream = patchWindowHistoryApiAndGetEventStream();
 
-  get urlChange() {
+  get urlChange(): Observable<RouterEvent> {
     return this.stream.asObservable().pipe(distinctUntilChanged(isSameUrl));
   }
 }
