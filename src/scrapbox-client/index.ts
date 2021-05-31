bd4db719d516b29d529a9a17427b1919c6daa6b6
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2021-05-31 20:24:19 +0900

    feat(scrapbox-client): refine api interface

--- src/scrapbox-client/index.ts
+++ src/scrapbox-client/index.ts
@@ -1,5 +1,4 @@
 export { ScrapboxClient } from './scrapbox-client';
-export { getScrapboxClient } from './get-singleton';
 export * from './rest-api-client/types';
 // TODO: improve param type to public
 export { ChangeRequestCreateParams } from './websocket-clinet/internal/request';
