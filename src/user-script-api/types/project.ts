1be95a15d21047f3bd82971c66dda72d59e4595f
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2021-06-04 20:25:41 +0900

    update

--- src/user-script-api/types/project.ts
+++ src/user-script-api/types/project.ts
@@ -1,22 +1,10 @@
-export type ExistentPageListItem = {
-  id: string;
-  title: string;
-  titleLc: string;
-  updated: number;
-  exists: true;
-};
-
-export type NonExistentPageListItem = {
-  id: undefined;
-  title: string;
-  titleLc: string;
-  updated: number;
-  exists: false;
-};
-
-export type ScrapboxProjectPage = ExistentPageListItem | NonExistentPageListItem;
-
 export type Project = {
   name: string;
-  pages: ScrapboxProjectPage[];
+  pages: {
+    id?: string; // undefined if exists === false
+    title: string;
+    titleLc: string;
+    updated: number;
+    exists: boolean;
+  }[];
 };
