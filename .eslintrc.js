f8fba45ed6a4ffa7e98882229286984967fff4c6
Kou Matsumoto <developer@kou-matsumoto.jp>
2021-08-25 19:09:30 +0900

    build: update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-var-requires': 'off', // for node-fetch
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'import/no-duplicates': 'error',
     'import/order': [
